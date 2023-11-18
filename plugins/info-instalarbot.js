7f85ca5e54705964b70802e34d806b2e0ce3a949
NayeyEmer13 <149829451+NayeyEmer13@users.noreply.github.com>
2023-11-18 00:18:54 -0500

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -2,26 +2,8 @@ import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 let handler  = async (m, { conn }) => {
 let texto = `
 ╭─────────────┈⊷
-│ *INSTALACIÓN DE TURBOBOT-MD*
-╰┬────────────┈⊷
-┌┤ *REQUISITOS PARA LAS INSTALACION*
-┌┤➳ _Dudas: wa.me/51929972576
-┌┤➳ _Tutoríal: https://youtu.be/qRb9ElGT8mM_
-┌┤➳ _1 GB de almacenamiento_
-┌┤➳ _Termux: https://www.mediafire.com/file/3hsvi3xkpq3a64o/termux_118.apk/file_
-┌┤➳ _GitHub: https://github.com/Jxtxn17/TurboBot-MD_
-┌┤➳ _un whatsapp inmune (secundario)_
-┌┤➳ _un número victual_
-┌┤➳ _2 dispositivo o una PC para escanear_
-╰┬────────────┈⊷
-  │ *COMANDO DE INSTALACION TERMUX*
-  │> termux-setup-storage
-  │> apt-get update -y && apt-get upgrade -y
-  │> pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn
-  │> git clone https://github.com/Jxtxn17/TurboBot-MD && cd TurboBot-MD && yarn install && npm install 
-  │> ls 
-  │> npm start 
-  ╰────────────┈⊷` 
+│ *No seas rata bro , para comprar el bot o saber los precios comunícate con mi creador https://wa.me/522212009202*
+╰┬────────────┈⊷` 
 let aa = { quoted: m, userJid: conn.user.jid }
 let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: `${texto}`.trim(), contextInfo: { externalAdReply: { title: 'Jxtxn17 ©', body: null, thumbnail: imagen2, sourceUrl: 'https://github.com/Jxtxn17/TurboBot-MD' }, mentionedJid: [m.sender] }}}, aa)
 conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id, mentions: [m.sender] })  
