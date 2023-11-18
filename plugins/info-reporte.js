5843bf7b7bd2cef9cba080d9e6c0ebac20ac7908
NayeyEmer13 <149829451+NayeyEmer13@users.noreply.github.com>
2023-11-18 00:28:57 -0500

    Update info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -3,7 +3,7 @@ if (!text) throw `*[⚠️] ESCRIBE EL REPORTE*\n\n💡 EJEMPLO\n*${usedPrefix +
 if (text.length < 8) throw `⚠️ *Mínimo 10 caracteres para hacer El Reporte.*`
 if (text.length > 1000) throw `⚠️ *Máximo 1000 caracteres para hacer El Reporte.*`
 let teks = `*⚠️ 𝙍𝙀𝙋𝙊𝙍𝙏𝙀 ⚠️*\n*📞 𝙉𝙐́𝙈𝙀𝙍𝙊*\nWa.me/${m.sender.split`@`[0]}\n*📝 𝙈𝙀𝙉𝙎𝘼𝙅𝙀*\n${text}`
-conn.reply('51929972576@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
+conn.reply('522212009202@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
 contextInfo: {
 mentionedJid: [m.sender]
 }})
