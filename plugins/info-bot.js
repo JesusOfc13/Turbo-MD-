f441c253c7099f18f99796b2d6fadf5fbf09af23
JesusOfc13 <151809045+JesusOfc13@users.noreply.github.com>
2023-11-23 17:22:15 -0500

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 let handler = m => m
 handler.all = async function (m) {
-let bot = `*🚀 Hola soy Bot*\nEn que te puedo ayudar?`
+let bot = `*🚀 Hola usuario solicitaste ver los precios de Nightcore-Bot-MD*\n\n*En la venta tenemos lo siguiente:*\n\n*📌Bots permanentes\n📌Bots mensuales\n📌bots propios (instalancion apk)\n📌Bots propios (instalacion por pagina)\n\n*Si deseas ver información y precio de lo que deseas mandar lo soguiente:*\n*ejemplo 📝:*\n.botpermanente`
 let vn = [imagen1, imagen2, imagen3, imagen4, img1].getRandom()
 let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) { 
