8d09cf93e4b6e2dea02d569a86387260567b3759
JesusOfc13 <151809045+JesusOfc13@users.noreply.github.com>
2023-11-23 18:56:32 -0500

    Update buscador-ssweb.js

--- plugins/buscador-ssweb.js
+++ plugins/buscador-ssweb.js
@@ -1,12 +1,23 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, command, args }) => {
-let full = /f$/i.test(command)
-if (!args[0]) return conn.reply(m.chat, '*Porfavor ingresa un url de la página a la que se le tomara captura🔎*', m)
-let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
-conn.sendFile(m.chat, ss, 'error.png', url, m)
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
+if (!args[0]) throw `Ejemplos de uso: ${usedPrefix}${command} https://s.id phone on
+*Dispositivo:*
+desktop
+tablet
+phone
+
+*Modo alargado:*
+on
+off
+`
+let caption = `Resultado: ${args[0]}`
+let img = `https://hadi-api.herokuapp.com/api/ssweb?url=${args[0]}&device=${args[1]}&full=${args[2]}`
+await conn.sendButton(m.chat, caption, wm, img, [
+                ['Siguiente', `${usedPrefix + command}`]
+            ], m, fdoc)
 }
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
-handler.command = /^ssweb$/
+handler.command = /^ssweb2$/
 export default handler
