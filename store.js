ffd82e229193ed7f77ed6b6f03ab248be46f6d79
JesusOfc13 <151809045+JesusOfc13@users.noreply.github.com>
2023-11-22 23:44:39 -0500

    Update store.js

--- store.js
+++ store.js
@@ -12,7 +12,7 @@ global.__filename = function filename(pathURL = import.meta.url, rmPrefix = plat
 const __dirname = global.__dirname(import.meta.url)
 
 //Imagenes
-global.imagen1 = fs.readFileSync('./storage/logos/Menu1.jpg')
+global.imagen1 = fs.readFileSync('./Menu2.jpg')
 global.imagen2 = fs.readFileSync('./storage/logos/Menu2.jpg')
 global.imagen3 = fs.readFileSync('./storage/logos/Menu3.jpg')
 global.imagen4 = fs.readFileSync('./storage/logos/Menu4.jpg')
