481396b7fe7b79496a83a6f5a44eb4158b6c737c
JesusOfc13 <151809045+JesusOfc13@users.noreply.github.com>
2023-11-24 21:00:21 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -8,14 +8,14 @@ let stiker = false
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 
-if (!/webp|image|video/g.test(mime) && !text) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝙑𝙄𝘿𝙀𝙊 𝘾𝙊𝙉 ${usedPrefix + command}*`)
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙀𝙇 𝙑𝙄́𝘿𝙀𝙊 𝙉𝙊 𝙋𝙐𝙀𝘿𝙀 𝘿𝙐𝙍𝘼𝙍 𝙈𝘼𝙎 𝘿𝙀 7 𝙎𝙀𝙂𝙐𝙉𝘿𝙊𝙎*')
+if (!/webp|image|video/g.test(mime) && !text) return m.reply(`*[🛑] EL VÍDEO O IMAGEN PARA CONVERTIR EN STICKER? ${usedPrefix + command}*`)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply('*[🛑] NO SEAS PNDJ DÓNDE VISTE UN STICKER DE 15 SEGUNDOS*')
 
 if (/webp|image|video/g.test(mime)) {
 let img = await q.download?.()
 let out
 stiker = await sticker(img, false, global.packname, global.author)
-await conn.reply(m.chat, `_Calma crack estoy haciendo tu sticker 👏_\n\n_*Recuerda los stickersgif son de 6 segundos*_\n\n_*by TurboBot*_`, m)
+await conn.reply(m.chat, `_Esperame bro soy algo lento... 🐢_\n\n_*Recuerda tener paciencia evita ser baneado*_\n\n_*by TurboBot*_`, m)
   
 if (!stiker) {
 if (/webp/g.test(mime)) out = await webp2png(img)
