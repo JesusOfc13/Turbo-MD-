070d084f039145161e079520ffe18f3eb06d10ba
SamuraiMD <150971370+SamuraiMD@users.noreply.github.com>
2023-11-26 09:26:07 -1200

    Add files via upload

---
+++ plugins/grupo-Fem4.js
@@ -0,0 +1,30 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Fem4);
+};
+handler.command = /^(Fem4|fem4)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.Fem4 = `
+𝟰 𝗩𝗘𝗥𝗦𝗨𝗦 𝟰
+
+𝐇𝐎𝐑𝐀𝐑𝐈𝐎
+🇵🇪 𝐏𝐄𝐑𝐔 : 
+🇦🇷 𝐀𝐑𝐆𝐄𝐍𝐓𝐈𝐍𝐀 : 
+🇨🇱 𝐂𝐇𝐈𝐋𝐄 :  
+¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+
+🌸 ┇  
+🌸 ┇ 
+🌸 ┇ 
+🌸 ┇ 
+
+ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+
+🌸 ┇ 
+🌸 ┇
+
+*◤By : Alacran Bot ◢*
+`;
---
+++ plugins/grupo-Masc4.js
@@ -0,0 +1,30 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Masc4);
+};
+handler.command = /^(Masc4|masc4|liga)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.Masc4 = `
+𝟰 𝗩𝗘𝗥𝗦𝗨𝗦 𝟰
+
+𝐇𝐎𝐑𝐀𝐑𝐈𝐎
+🇵🇪 𝐏𝐄𝐑𝐔 : 
+🇦🇷 𝐀𝐑𝐆𝐄𝐍𝐓𝐈𝐍𝐀 : 
+🇨🇱 𝐂𝐇𝐈𝐋𝐄 :  
+¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+
+🥷🏻 ┇  
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+
+ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+
+*◤By : Alacran Bot ◢*
+`;
